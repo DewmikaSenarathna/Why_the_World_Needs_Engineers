1e4a5697cdb3d879602d0871e5e8828fce3580e1
DewmikaSenarathna <152563752+DewmikaSenarathna@users.noreply.github.com>
2024-05-25 15:29:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleDropdown() {
+    var dropdown = document.getElementById("dropdown");
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+}
